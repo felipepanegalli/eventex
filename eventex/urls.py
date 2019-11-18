5c0c612bf7afcc4845bfe3e159b0a0cba11f775b
Felipe Panegalli <felipe.panegalli@gmail.com>
2019-11-18 14:59:53 -0300

    Adicionado test na home

--- eventex/urls.py
+++ eventex/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+
 import eventex.core.views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', eventex.core.views.home),
     path('admin/', admin.site.urls),
 ]
-
-urlpatterns += [] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
