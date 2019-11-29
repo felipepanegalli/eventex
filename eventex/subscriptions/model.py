0aac6acd2eb1b2e5718f2ac0a87ec212bbeb0d57
Felipe Panegalli <felipe.panegalli@gmail.com>
2019-11-29 15:18:36 -0300

    Alterado campo de CPF

--- eventex/subscriptions/model.py
+++ eventex/subscriptions/model.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Subscription(models.Model):
     name = models.CharField(max_length=100, verbose_name='Nome')
-    cpf = models.CharField(max_length=13, verbose_name='CPF')
+    cpf = models.CharField(max_length=14, verbose_name='CPF')
     email = models.EmailField(verbose_name='E-mail')
     phone = models.CharField(max_length=20, verbose_name='Telefone')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Criado em')
