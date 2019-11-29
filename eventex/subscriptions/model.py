1f0acfb7cdad79454c2938383218bf934e923a8f
Felipe Panegalli <felipe.panegalli@gmail.com>
2019-11-29 15:06:59 -0300

    Customizado o admin do subscription

--- eventex/subscriptions/model.py
+++ eventex/subscriptions/model.py
@@ -2,8 +2,16 @@ from django.db import models
 
 
 class Subscription(models.Model):
-    name = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=13)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100, verbose_name='Nome')
+    cpf = models.CharField(max_length=13, verbose_name='CPF')
+    email = models.EmailField(verbose_name='E-mail')
+    phone = models.CharField(max_length=20, verbose_name='Telefone')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Criado em')
+
+    class Meta:
+        verbose_name = 'Inscrição'
+        verbose_name_plural = 'Inscrições'
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.name
