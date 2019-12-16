f37cdeb01a102d83e45a4129cce2275a4f52178b
Felipe Panegalli <felipe.panegalli@gmail.com>
2019-12-16 17:22:55 -0300

    Criado o model de Palestrantes

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.shortcuts import resolve_url as r
+
 
 # Create your models here.
+
+class Speaker(models.Model):
+    name = models.CharField('Nome', max_length=255)
+    slug = models.SlugField()
+    photo = models.URLField('Foto')
+    website = models.URLField('Site', blank=True)
+    description = models.TextField('Sobre mim', blank=True)
+
+    class Meta:
+        verbose_name = 'palestrante'
+        verbose_name_plural = 'palestrantes'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return r('speaker_detail', slug=self.slug)
+
+
--- eventex/urls.py
+++ eventex/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-import eventex.core.views
+from eventex.core.views import home, speaker_detail
 
 urlpatterns = [
-    path('', eventex.core.views.home, name='home'),
-    path('inscricao/', include('eventex.subscriptions.urls')),
+    path('', home, name='home'),
+    path('inscricao/', include('eventex.subscriptions.urls', namespace='subscriptions')),
+    path('palestrantes/<slug:slug>/', speaker_detail, name='speaker_detail'),
     path('admin/', admin.site.urls),
 ]
