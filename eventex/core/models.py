d123beec90e17a5bc224046e309d673300668923
Felipe Panegalli <felipe.panegalli@gmail.com>
2019-12-16 20:01:16 -0300

    Criado o managers para uso de querys - Talks

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.shortcuts import resolve_url as r
 
+from eventex.core.managers import KindQuerySet, PeriodManager
 
-# Create your models here.
 
 class Speaker(models.Model):
     name = models.CharField('Nome', max_length=255)
@@ -34,6 +34,8 @@ class Contact(models.Model):
     kind = models.CharField('tipo', max_length=1, choices=KINDS)
     value = models.CharField('valor', max_length=255)
 
+    objects = KindQuerySet.as_manager()
+
     class Meta:
         verbose_name = 'contato'
         verbose_name_plural = 'contatos'
@@ -47,6 +49,8 @@ class Talk(models.Model):
     start = models.TimeField('Horário', blank=True, null=True)
     description = models.TextField('Descrição', blank=True)
     speakers = models.ManyToManyField('Speaker', verbose_name='Palestrante(s)', blank=True)
+
+    objects = PeriodManager()
 
     class Meta:
         verbose_name = 'palestra'
