fd68a55c84a65ddc26520962fbb7b8cb80882adb
Felipe Panegalli <felipe.panegalli@gmail.com>
2019-12-16 11:32:34 -0300

    Criado testes e implementado os lotes de pago

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -1,8 +1,11 @@
+from unittest.mock import Mock
+
 from django.core import mail
 from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
 from eventex.subscriptions.model import Subscription
 from django.shortcuts import resolve_url as r
+from eventex.subscriptions.admin import SubscriptionModelAdmin, admin
 
 
 class SubscribeTest(TestCase):
@@ -118,3 +121,30 @@ class SubscriptionModelTest(TestCase):
 
     def test_paid_default_to_false(self):
         self.assertEqual(False, self.obj.paid)
+
+
+class SubscriptionAdmin(TestCase):
+    def setUp(self):
+        Subscription.objects.create(name="Felipe Panegalli", cpf="12312312345", email="felipe.panegalli@gmail.com",
+                                    phone="12-12345-1234")
+        self.model_admin = SubscriptionModelAdmin(Subscription, admin.site)
+
+    def test_has_action(self):
+        self.assertIn('mark_as_paid', self.model_admin.actions)
+
+    def test_mark_all(self):
+        self.call_action()
+        self.assertEqual(1, Subscription.objects.filter(paid=True).count())
+
+    def test_message(self):
+        mock = self.call_action()
+        mock.assert_called_once_with(None, '1 inscrição foi marcada como paga.')
+
+    def call_action(self):
+        queryset = Subscription.objects.all()
+        mock = Mock()
+        old_message_user = SubscriptionModelAdmin.message_user
+        SubscriptionModelAdmin.message_user = mock
+        self.model_admin.mark_as_paid(None, queryset)
+        SubscriptionModelAdmin.message_user = old_message_user
+        return mock
