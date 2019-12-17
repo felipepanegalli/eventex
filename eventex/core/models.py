13f7f4c7cdeb6f5481dcbf319579002635e583d3
Felipe Panegalli <felipe.panegalli@gmail.com>
2019-12-17 11:18:31 -0300

    Course como MTI

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -44,7 +44,7 @@ class Contact(models.Model):
         return self.value
 
 
-class Activity(models.Model):
+class Talk(models.Model):
     title = models.CharField('Título', max_length=200)
     start = models.TimeField('Horário', blank=True, null=True)
     description = models.TextField('Descrição', blank=True)
@@ -53,7 +53,7 @@ class Activity(models.Model):
     objects = PeriodManager()
 
     class Meta:
-        abstract = True
+        ordering = ['start']
         verbose_name = 'palestra'
         verbose_name_plural = 'palestras'
 
@@ -61,12 +61,10 @@ class Activity(models.Model):
         return self.title
 
 
-class Talk(Activity):
-    pass
-
-
-class Course(Activity):
+class Course(Talk):
     slots = models.IntegerField('Vagas', blank=True)
+
+    objects = PeriodManager()
 
     class Meta:
         verbose_name = 'curso'
